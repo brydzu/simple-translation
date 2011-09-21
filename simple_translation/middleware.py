a272f886867e702354c18f283e58b4548d731147
fivethreeo <oyvind.saltvik@gmail.com>
2011-09-21 23:44:12 +0200

    refactor middleware, ready for starting class based views support

--- simple_translation/middleware.py
+++ simple_translation/middleware.py
@@ -4,8 +4,7 @@ from django.utils import translation
 
 from simple_translation.translation_pool import translation_pool
 
-def filter_queryset_language(request, queryset):
-    language = getattr(request, 'LANGUAGE_CODE')
+def filter_queryset_language_code(request, queryset, language):
     model = queryset.model
     filter_expr = None
     if translation_pool.is_registered(model):
@@ -16,13 +15,18 @@ def filter_queryset_language(request, queryset):
         filter_expr = '%s' % info.language_field
     if filter_expr:
         queryset = queryset.filter( \
-            **{filter_expr: language}).distinct()
+            **{filter_expr: language}).distinct()            
     return queryset
     
-class MultilingualGenericsMiddleware(LocaleMiddleware):
+def filter_queryset_language(request, queryset):
+    language = getattr(request, 'LANGUAGE_CODE')
+    return filter_queryset_language_code(request, queryset, language)
     
+class BaseMultilingualMiddleware(LocaleMiddleware):
+    
+    is_generic_middleware = False
     language_fallback_middlewares = ['django.middleware.locale.LocaleMiddleware']
-    
+ 
     def has_language_fallback_middlewares(self):
         has_fallback = False
         for middleware in self.language_fallback_middlewares: 
@@ -30,6 +34,23 @@ class MultilingualGenericsMiddleware(LocaleMiddleware):
                 has_fallback = True
         return has_fallback
 
+    def process_response(self, request, response):
+        if not self.has_language_fallback_middlewares():
+            return super(MultilingualGenericsMiddleware, self).process_response(request, response)
+        return response
+        
+class FilterQuerysetMixin(object):
+    """ Filters any queryset passed to the view based on request.LANGUAGE_CODE """
+    
+    is_generic_middleware = True
+    
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if 'queryset' in view_kwargs:
+            view_kwargs['queryset'] = filter_queryset_language(request, view_kwargs['queryset'])  
+
+class MultilingualUrlMixin(object):
+    """ Checks for language_code and in view_kwargs, sets language, call super depending on is_generic_middleware """
+    
     def process_view(self, request, view_func, view_args, view_kwargs):
         language = None
         if 'language_code' in view_kwargs:
@@ -37,11 +58,30 @@ class MultilingualGenericsMiddleware(LocaleMiddleware):
             language = view_kwargs.pop('language_code')
             translation.activate(language)
             request.LANGUAGE_CODE = translation.get_language()
+            
+        if self.is_generic_middleware:
+            super(MultilingualUrlMixin, self).process_view(request, view_func, view_args, view_kwargs):
+            
+class MultilingualUrlMiddleware(BaseMultilingualMiddleware, MultilingualUrlMixin):
+    """ Checks for language_code and in view_kwargs, sets language """
+    pass
+    
+class MultilingualNoUrlGenericsMiddleware(BaseMultilingualMiddleware, FilterQuerysetMixin):
+    """ Filters any queryset passed to the view based on request.LANGUAGE_CODE """
+    pass
+    
+class CmsMultilingualGenericsMiddleware(MultilingualNoUrlGenericsMiddleware):
+    """ Filters any queryset passed to the view based on request.LANGUAGE_CODE tests for django-cms middleware"""
+    
+    language_fallback_middlewares = [
+        'django.middleware.locale.LocaleMiddleware',
+        'cms.middleware.multilingual.MultilingualURLMiddleware'
+    ]
 
-        if 'queryset' in view_kwargs:
-            view_kwargs['queryset'] = filter_queryset_language(request, view_kwargs['queryset'])  
+class MultilingualUrlGenericsMiddleware(MultilingualGenericsMiddleware, MultilingualUrlMixin):
+    """ Checks for language_code in view_kwargs, sets language and filters any queryset passed to the view by request.LANGUAGE_CODE """
+    pass
 
-    def process_response(self, request, response):
-        if not self.has_language_fallback_middlewares():
-            return super(MultilingualGenericsMiddleware, self).process_response(request, response)
-        return response
+class MultilingualGenericsMiddleware(MultilingualUrlGenericsMiddleware):
+    """ For backwards compatibility """
+    pass
